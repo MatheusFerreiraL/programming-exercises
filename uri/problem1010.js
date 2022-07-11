791a1a023e32ae8d44cc876be530190f5ae448f0
MatheusFerreiraL <matheusfleandro@gmail.com>
2022-07-10 23:28:19 -0300

    problem 1010 - solved

--- uri/problem1010.js
+++ uri/problem1010.js
@@ -1,12 +1,18 @@
 var input = require("fs").readFileSync("stdin", "utf8");
 var lines = input.split("\n");
 
-let codigo1 = parseInt(lines.shift());
-let codigo2 = parseInt(lines.shift());
-let qtdPeca1 = parseInt(lines.shift());
-let qtdPeca2 = parseInt(lines.shift());
-let valorPeca1 = parseFloat(lines.shift());
-let valorPeca2 = parseFloat(lines.shift());
+var item1 = lines.shift().split(" ");
+var item2 = lines.shift().split(" ");
 
-let valorTotal = qtdPeca1 * valorPeca1 + qtdPeca2 * valorPeca2;
-console.log("VALOR A PAGAR: R$ " + valorTotal);
+console.log("Item 1: ", item1);
+console.log("Item 2: ", item2);
+
+let idItem1 = item1.shift();
+let qtdItem1 = item1.shift();
+let valorItem1 = item1.shift();
+let idItem2 = item2.shift();
+let qtdItem2 = item2.shift();
+let valorItem2 = item2.shift();
+let valorTotal = qtdItem1 * valorItem1 + qtdItem2 * valorItem2;
+
+console.log("VALOR A PAGAR: R$ " + valorTotal.toFixed(2));
